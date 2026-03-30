92bf768b3967f1c947ca0b5f052e523227657252
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2026-03-30 13:46:25 +0530

    fix:sidebar moving

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -6,7 +6,7 @@ import Sidebar from "@/components/sidebar";
 const Explore = () => {
   
   return (
-    <div className="sm:w-screen min-h-screen flex overflow-hidden bg-black text-white">
+    <div className="sm:w-screen h-screen flex overflow-hidden bg-black text-white">
       <Sidebar />
       <div className="w-full flex flex-col min-h-screen">
         <div className="flex-col w-full gap-y-1 flex items-center justify-center">
